079735e4ee548fd3dea0272419a81a513240d1ea
[AlenBiz] <[jobbizyaev@gmail.com]>
2022-12-14 09:54:46 +0100

    added a first test cypress

---
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe.only("Testing Header", () => {
+  beforeEach("Visit", () => {
+    cy.visit('https://staging.lpitko.ru/');
+  });
+
+  it("header have LogIn", () => {
+    cy.get(
+      '#root > div.layout-1 > section.layout-1__header-wrapper-fixed > header > section > div > a > div > div > span'
+    ).should('have.text', 'Вход и регистрация');
+  });
+  it("header have santa", () => {
+    cy.get(
+      '#root > div.layout-1 > section.layout-1__header-wrapper.layout-1__header-wrapper--fixed > header > section > span'
+    ).should('have.text', '19 Сант в этом году');
+  });
+});
